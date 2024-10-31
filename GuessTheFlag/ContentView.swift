e89fc2cf046e1398ca4a17caba39d61583c62a84
Martin Flischman <martincodes@icloud.com>
2024-10-31 09:13:43 +0200

    Game Logic Implementation with Score Feedback
    feat: add game logic and score alerts
    
    - Add flagTapped function to check correct/wrong answers
    - Implement askQuestion function to shuffle flags and reset game
    - Add score alert with continue button
    - Make countries and correctAnswer state variables

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
-    var correctAnswers = Int.random(in: 0...2)
-
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
     
@@ -24,19 +23,39 @@ struct ContentView: View {
                     Text("Tap the flag of")
                         .foregroundStyle(.white)
                     
-                    Text(countries[correctAnswers])
+                    Text(countries[correctAnswer])
                         .foregroundStyle(.white)
                 }
                 
                 ForEach(0..<3) { number in
                     Button {
-                        
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                     }
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
